f2b6e8a0c791797af42dad10064c77c5d9c120fa
Soul-Charge <xiaoyaochn@outlook.com>
2020-02-10 07:18:06 +0800

    完成第三章第四题

---
+++ code/Chapter_3/3.11.4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float n;
+    scanf("%f", &n);
+    printf("The input is %f or %e.\n",n,n);
+
+    system("pause");
+    return 0;
+}
