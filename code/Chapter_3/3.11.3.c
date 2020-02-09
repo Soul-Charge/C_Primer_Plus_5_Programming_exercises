187a7b66f095145c3107d6c84da3193f503215fb
Soul-Charge <xiaoyaochn@outlook.com>
2020-02-10 07:09:47 +0800

    完成第三章第三题

---
+++ code/Chapter_3/3.11.3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    putchar('\a');
+    puts("Startled by the sudden sound, Sally shouted, \"By the Great Pumpkin, what was that ! \"");
+
+    system("pause");
+    return 0;
+}
