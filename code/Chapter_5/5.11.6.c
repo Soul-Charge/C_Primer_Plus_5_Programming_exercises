301720ec5d07da5ae438162c3885a7cfa6533a26
Soul-Charge <xiaoyaochn@outlook.com>
2020-02-17 09:38:02 +0800

    完成第五章第六题

---
+++ code/Chapter_5/5.11.6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int count, sum, max;
+    count = sum = 0;
+    max = 20;
+
+    printf("How many numbers?: ");
+    scanf("%d", &max);
+
+    while(count++ < max)
+        sum = sum + count*count;
+    printf("sum = %d\n", sum);
+
+    system("pause");
+    return 0;
+}
