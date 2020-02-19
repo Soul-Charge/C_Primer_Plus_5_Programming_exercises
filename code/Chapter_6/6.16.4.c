529d29b4b93f8d0995856a9847142ee138275cc2
Soul-Charge <xiaoyaochn@outlook.com>
2020-02-19 19:09:50 +0800

    完成彩虹效果(效果偏差，cmd窗口只能修改前景色？)

--- code/Chapter_6/6.16.4.c
+++ code/Chapter_6/6.16.4.c
@@ -2,15 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#define LINESIZE 20 //命令行的长度
+
+bool colorON = true;
 
 int main(void)
 {
     char letter; //用户输入的字符
     char * letterPos; //输入字符在列表位置的指针
-    const char charList[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    char colorList[] = {"4e62915"}; //cmd的颜色序列码，按照彩虹颜色顺序
+    //const char charList[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     //const char charList[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     //const char charList[] = {"1234567890"};
-    //const char charList[] = {"1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    const char charList[] = {"1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
 
 
     printf("Enter a letter: ");
@@ -22,8 +27,23 @@ int main(void)
         return 0;
     }
 
+    int currentColor = 0; //当前的颜色
+    int lines = (int)(letterPos - charList); //计算总共将要打印的行数
+    int divideVal = lines / 7; //变换一次颜色的行数
+    if (lines < 7) //行数小于7关闭效果
+        colorON = false;
+
     for (int rows = 0; charList+rows <= letterPos; rows++)
     {
+        /* 切换当前颜色 */
+        if (rows != 0 && rows % divideVal == 0)
+            currentColor++;
+
+        /* 生成与执行颜色切换命令 */
+        char colorLine[LINESIZE]; //存储命令行
+        sprintf(colorLine, "color %c", colorList[currentColor]);
+        system(colorLine);
+
         /* 处理空格 */
         for (int spaceNum = rows; charList+spaceNum < letterPos; spaceNum++)
             putchar(' ');
