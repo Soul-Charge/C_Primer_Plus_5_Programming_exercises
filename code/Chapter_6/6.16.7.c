a0414ec66256362f6bcd46cc659c29e00aa40c45
Soul-Charge <xiaoyaochn@outlook.com>
2020-03-04 08:59:10 +0800

    完成第六章第七题

---
+++ code/Chapter_6/6.16.7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    double val1;
+    double val2;
+
+    printf("Enter two floating number(eg: 23.1 13.4):");
+    while (scanf("%lf %lf", &val1, &val2) == 2)
+    {
+        printf("(%.3lf-%.3lf) / (%.3lf*%.3lf)=%.3lf\n"
+        ,val1, val2, val1, val2, (val1 - val2) / (val1 * val2));
+
+        printf("Enter two floating number(eg: 23.1 13.4)(q to quit):");
+    }
+
+    system("pause");
+    return 0;
+}
