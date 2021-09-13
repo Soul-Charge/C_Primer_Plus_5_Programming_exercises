d0ed25d32ad7a5bc7d0a34591cc9b51f36a47249
Soul-Charge <xiaoyaochn@outlook.com>
2021-09-13 22:55:23 +0800

    修改2.12.3.c并添加2.12.3.md

--- code/Chapter_2/2.12.3.c
+++ code/Chapter_2/2.12.3.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define AGE 17
 
 int main(void)
 {
-    unsigned short age = AGE;
-    printf("Age:%5d\n", age);
-    printf("Days:%5d\n", age * 365);
+    printf("My Age is %d, it equals %d days.\n", 18, 18 * 365);
     system("pause");
     return 0;
 }
