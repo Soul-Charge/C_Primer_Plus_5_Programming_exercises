5eaa91b528daf288a78d428a876144cc95ba748c
Soul-Charge <xiaoyaochn@outlook.com>
2021-09-14 14:12:18 +0800

    修改2.12.6.c并添加2.12.6.md

--- code/Chapter_2/2.12.6.c
+++ code/Chapter_2/2.12.6.c
@@ -8,7 +8,7 @@ void putMsg(void)
 
 int main(void)
 {
-    /* version 1: 简单无脑版
+    /* version 1: 简单无脑版 */
     putMsg();
     putMsg(); 
     putMsg();
@@ -18,7 +18,6 @@ int main(void)
     printf("\n");
     putMsg();
     printf("\n");
-    */
 
     /* version 2: 简单无脑整齐版
     putMsg();putMsg();putMsg();putchar('\n');
@@ -26,13 +25,14 @@ int main(void)
     putMsg();putchar('\n');
     */
     
-    /* version 3: 精简版 */
+    /* version 3: 精简版 
     for (int i = 3; i > 0;i--) //控制打印消息的行数
     {
         for (int j = 0; j < i;j++) //控制每行的列数
             putMsg();              //打印的消息
         putchar('\n');
     }
+    */
 
     system("pause");
     return 0;
