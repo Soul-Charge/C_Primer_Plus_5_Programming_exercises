e6703b25ce86750f5f16535bc735b5dfbbf3aa64
Soul-Charge <xiaoyaochn@outlook.com>
2021-09-14 14:04:54 +0800

    修改2.12.5.c并添加2.12.5.md

--- code/Chapter_2/2.12.5.c
+++ code/Chapter_2/2.12.5.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TOES 10
 
 int main(void)
 {
-    unsigned short toes = TOES;
+    int toes;
 
-    printf("toes=%14d\n", toes);
-    printf("sum of two toes=%3d\n", toes + toes);
-    printf("square of toes=%4d\n", toes * toes);
+    toes = 10;
+    printf("toes = %d\n", toes);
+    printf("toes + toes = %d\n", toes + toes);
+    printf("toes * toes = %d\n", toes * toes);
 
     system("pause");
     return 0;
